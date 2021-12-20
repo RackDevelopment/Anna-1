cc4f9f6dd167102f1fa75dee5ca99b51b32a0836
Optic_Fusion1 <37254722+OpticFusion1@users.noreply.github.com>
2021-12-20 05:21:15 -0500

    Version 0.1.0 Adds Main Adds Anna

---
+++ src/main/java/optic_fusion1/anna/Anna.java
@@ -0,0 +1,9 @@
+package optic_fusion1.anna;
+
+public class Anna extends Thread {
+
+  @Override
+  public void run() {
+  }
+
+}
---
+++ src/main/java/optic_fusion1/anna/Main.java
@@ -0,0 +1,9 @@
+package optic_fusion1.anna;
+
+public class Main {
+
+  public static void main(String[] args) {
+    new Anna().start();
+  }
+
+}
